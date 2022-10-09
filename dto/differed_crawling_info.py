8808bc1fc83a6a203d45d732c301631c6ebf3a9f
Jeremias Pfluger <jeremiaspfluger@Jeremias-Pfluger.local>
2022-10-09 16:16:33 +0200

    removing unnecesary parameter

--- dto/differed_crawling_info.py
+++ dto/differed_crawling_info.py
@@ -1,15 +1,14 @@
 class DifferedCrawlingInfo:
     def __init__(self, browser_type, bot_name, crawl_url, information_selector_type, html_information_location,
-                 html_information_attribute_identifier, html_items_per_line, inner_html_information_location,
-                 next_page_policy, next_page_selector_type, next_page_location, complete_crawl_url,
-                 scrolling_needs_click, scrolling_timeout, parsing_items):
+                 html_information_attribute_identifier, inner_html_information_location, next_page_policy,
+                 next_page_selector_type, next_page_location, complete_crawl_url, scrolling_needs_click,
+                 scrolling_timeout, parsing_items):
         self.browser_type = browser_type
         self.bot_name = bot_name
         self.crawl_url = crawl_url
         self.information_selector_type = information_selector_type
         self.html_information_location = html_information_location
         self.html_information_attribute_identifier = html_information_attribute_identifier
-        self.html_items_per_line = html_items_per_line
         self.inner_html_information_location = inner_html_information_location
         self.next_page_policy = next_page_policy
         self.next_page_selector_type = next_page_selector_type
